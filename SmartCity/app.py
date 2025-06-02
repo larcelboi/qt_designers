43c900e29f20f1d858ce6bb95de77db0e5d759ec
larcel <2355315@cegepoutaouais.qc.ca>
2025-06-02 14:11:46 -0400

    sad

--- SmartCity/app.py
+++ SmartCity/app.py
@@ -1,3 +1,25 @@
 # Main application entry
+import sys
+
+from PySide6.QtWidgets import QMainWindow, QApplication
 
 # TODO: Load UI, handle roles, connect actions
+from interface.ui_main_window import Ui_MainWindow
+from interface.ui_add_device_dialog import Ui_AddDeviceDialog
+from interface.ui_add_district_dialog import Ui_AddDistrictDialog
+from interface.ui_malfunction_dialog import Ui_MalfunctionDialog
+
+class RunApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = RunApp()
+    window.show()
+    sys.exit(app.exec())
--- SmartCity/interface/Ui_MainWindow.py
+++
@@ -1,1 +0,0 @@
-# Placeholder for main window UI code (from Qt Designer)
